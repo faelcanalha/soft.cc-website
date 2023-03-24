7e58b553b9efb9a88eee67e0148305dc94ad54b7
fael <4afa3lbr@gmail.com>
2023-03-24 19:05:02 -0300

    add proxy

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,16 +1,30 @@
 function NavBar() {
-    return (
-        <nav className="navbar">
-            <div className="nav">
-                <a className="nav-brand" href="/">SOFT</a>
-                <ul>
-                    <li><a href="#home"><span className=''>Home</span></a></li>
-                    <li><a href="#prices"><span className=''>Prices</span></a></li>
-                    <li><a href="/login"><span className=''>Login</span></a></li>
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="nav">
+        <a className="nav-brand" href="/">
+          SOFT
+        </a>
+        <ul>
+          <li>
+            <a href="#home">
+              <span className="">Home</span>
+            </a>
+          </li>
+          <li>
+            <a href="#prices">
+              <span className="">Prices</span>
+            </a>
+          </li>
+          <li>
+            <a href="/login">
+              <span className="">Login</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
